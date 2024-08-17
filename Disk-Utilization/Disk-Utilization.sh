ac5b9974785cfae30a5e76fbb92b7b722e3d8a13
AB_GOWDA <abhishekgowda8090@gmail.com>
2024-08-17 10:13:31 +0530

    This will check the Disk-utilization in the System

---
+++ Disk-Utilization/Disk-Utilization.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+##################################################################################################
+#
+# Author : Abhishek Gowda HC
+# Date : 17/08/2024
+#
+# This Script will check the Disk utilization ,Shows how much it is utlized and remaing free-disk system.
+#
+######################################################################################################
+
+echo"Check the Disk Usage in Linux System"
+
+disk_size ='df -h | grep "/../.../../.." | awk '{pritn 5}' |cut -d '%' -f1'
+
+if [disk_size -gt 80];
+then
+    echo"Disk is utilized more than 80%,Expand disk or delete files soon"
+else 
+    echo"Enough disk is Available"
+fi
+
+
---
+++ Disk-Utilization/README.md
@@ -0,0 +1,5 @@
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Disk-Utilization>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+This script will shows the Disk Utilization in the given Disk path.
+It will Shows how much free-Disk-Space remaing and Utilized-Disk-Space. 
